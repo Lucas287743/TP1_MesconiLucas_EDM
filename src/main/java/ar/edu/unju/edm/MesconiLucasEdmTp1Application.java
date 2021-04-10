40f7cd6ca671a2f8a97289ef7c81a89bc36e6329
mesco <mesco@LAPTOP-GDTTGJ9M>
2021-04-10 11:30:40 -0300

    Mesconi Lucas

---
+++ src/main/java/ar/edu/unju/edm/MesconiLucasEdmTp1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesconiLucasEdmTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesconiLucasEdmTp1Application.class, args);
+	}
+
+}
